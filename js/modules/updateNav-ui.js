4dc32a83de02be9b924307f0badd7c25cbe3a4f4
Amel Ouferhat <ouferhatamel@gmail.com>
2022-05-27 01:46:54 +0100

    Adding the mobile menu

--- js/modules/updateNav-ui.js
+++ js/modules/updateNav-ui.js
@@ -17,3 +17,22 @@ onAuthStateChanged(auth, user => {
         login.style.display = 'inline';
     }
 });
+
+//Burger menu
+const burger = document.querySelector('.burger');
+let navBarLinks = document.querySelector('.navBar__links');
+let menuOpen = false;
+
+burger.addEventListener('click', () => {
+    if(!menuOpen){
+        burger.classList.add('burger__bar--open');
+        menuOpen = true;
+        navBarLinks.style.clipPath = "circle(100%)";
+        navBarLinks.style.display = 'flex';
+    }else{
+        burger.classList.remove('burger__bar--open');
+        menuOpen = false;
+        navBarLinks.style.clipPath = "circle(0% at 50% -50%)";
+        navBarLinks.style.display = 'none'
+    }
+});
